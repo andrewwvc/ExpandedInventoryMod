e96c84d552c01bfcb674880357e320f7b6ae8a10
andrewwvc <andrewwvc@gmail.com>
2026-01-27 10:07:40 +1100

    Extracted common parts into header

--- src/bomb_chu_change.c
+++ src/bomb_chu_change.c
@@ -1,84 +1,20 @@
-#include "modding.h"
-#include "global.h"
-#include "recomputils.h"
-#include "recompconfig.h"
-#include "eztr_api.h"
+#include "chromeless_item_extension_common.h"
 
-#include "overlays/actors/ovl_En_Box/z_en_box.h"
 #include "overlays/actors/ovl_En_Bom_Chu/z_en_bom_chu.h"
 #include "overlays/actors/ovl_En_Bom/z_en_bom.h"
-#include "overlays/actors/ovl_En_Boom/z_en_boom.h"
 
 #include "object_gi_insect_custom.h"
 
-#include "overlays/kaleido_scope/ovl_kaleido_scope/z_kaleido_scope.h"
 
-#include "z64message.h"
-#include "z64player.h"
-//#include "assets/objects/gameplay_keep/gameplay_keep.h"
+RECOMP_IMPORT(".", s16 ItemExtension_ToNewItemRange(s16 itemID));
+RECOMP_IMPORT(".", s16 ItemExtension_FromItemRangeToItemID(s16 itemID));
 
-typedef void (*PlayerItemActionInitFunc)(PlayState*, Player*);
+RECOMP_IMPORT(".", NewItemNum InitializeNewItemFromEntry(CustomItemEntry* entry));
 
-s32 Player_UpperAction_CarryActor(Player* this, PlayState* play);
-s32 Player_UpperAction_ChangeHeldItem(Player* this, PlayState* play);
+RECOMP_IMPORT(".", s32 ItemExtension_OfferExtendedGetItem(Actor* actor, PlayState* play, s16 getItemIdEx, f32 xzRange, f32 yRange));
+RECOMP_IMPORT(".", s32 ItemExtension_OfferExtendedGetItemFar(Actor* actor, PlayState* play, s16 getItemIdEx));
+RECOMP_IMPORT(".", s32 ItemExtension_OfferExtendedGetItemUnconditional(Actor* actor, PlayState* play, s16 getItemIdEx));
 
-#define NewItemNum s16
-
-typedef struct {
-    /* 0x0 */ void (*drawFunc)(PlayState*, s16);
-    /* 0x4 */ void* drawResources[8]; // Either display lists (Gfx*) or matrices (Mtx*)
-} DrawItemTableEntry;                 // size = 0x24
-
-typedef enum {
-    IT_CUSTOM = 0,
-    IT_MELEE_1H,
-    IT_MELEE_2H,
-    IT_USE,         //Immediately used items, like thrown Deku nuts
-    IT_BOTTLED,
-    IT_TRADE,
-    IT_VIEWFINDER,  //Items that you 'look through', e.g. pictograph, telescope
-    IT_PROJECTILE,  //Objects that 'shoot out of' something, uses first person + Z-targeting
-    IT_EXPLOSIVE,   //Actually just any items that produce something 'held above your head'
-    IT_SPELL,       //Magic with a casting animation
-    IT_MASK,
-    IT_TRANSFORMATION_MASK
-} ItemType;
-
-typedef enum {
-    UI_OOT_MM,
-    UI_OOT_ONLY,
-    UI_MM_ONLY
-} GamesUsedIn;
-
-typedef enum {
-    SA_AUTO_EMPTY = -1,     //for blank slots put there so the items can be collected later
-    SA_AUTO_PREFILL = -2,   //for the slot to be filled with the item unconditionally
-    SA_NONE = -3            //provides no slot assignment, mod should assign item using custom code
-} SlotAssignment;
-
-typedef struct {
-    PlayerItemActionInitFunc initFunc; //Used to initialize the item action
-    PlayerUpperActionFunc actionFunc;  //The item acton's update code
-    u8 modelGroup;                      //Should be a valid PlayerModelGroup
-    s16 slotAssignment;         //The inventory slot position number the item is to be assigned, otherwise SA_NONE gives no slot-
-                                //for moders who want to deal with that themselves, while SA_AUTO_(EMPTY/PREFILL) generates a new one
-} InventoryItemPerGameEntry;
-
-typedef struct {
-    char* itemName;             //Name the item will be refered to as internally, should be unique
-    GamesUsedIn usedIn;         //Will this item be used in OoT, MM or both?
-    TexturePtr icon;            //Pointer to RGBA32 Texture
-    TexturePtr nameLabelEng;    //Pointer to IA8 Texture
-
-    char* EZTR_KaleidoPopupText;        //Used in MM by any item assigned to a slot
-    char* EZTR_GiveItemText;            //Used for givables
-    InventoryItemPerGameEntry ocarinaFuncs;
-    InventoryItemPerGameEntry mujuraFuncs;
-    ItemType type;                      //optional, provides information to the integration code
-    DrawItemTableEntry* drawEntryGI;    //Needed for givable/tradable items, otherwise optional
-    s16 salePrice;                      //defaults to 0, meaning the item will not be sold, other numbers mean shops will accept it
-    char* metadata;                     //optional, arbitrary null terminated data used to communicate with other mods
-} CustomItemEntry;
 
 RECOMP_PATCH void EnBomChu_Move(EnBomChu* this, PlayState* play) {
     CollisionPoly* polySide = NULL;
@@ -442,98 +378,14 @@ CustomItemEntry bombmineEntry = {
     .type = IT_EXPLOSIVE,
 };
 
-typedef struct GetItemEntry {
-    /* 0x0 */ u8 itemId;
-    /* 0x1 */ u8 field; // various bit-packed data
-    /* 0x2 */ s8 gid;   // defines the draw id and chest opening animation
-    /* 0x3 */ u8 textId;
-    /* 0x4 */ u16 objectId;
-} GetItemEntry; // size = 0x6
-
-#define IDLE_ANIM_NONE 0
-#define CHEST_ANIM_SHORT 0
-#define CHEST_ANIM_LONG 1
-
-// TODO: consider what to do with the NONEs: cannot use a zero-argument macro like OoT since the text id is involved.
-#define GET_ITEM(itemId, objectId, drawId, textId, field, chestAnim) \
-    { itemId, field, (chestAnim != CHEST_ANIM_SHORT ? 1 : -1) * (drawId + 1), textId, objectId }
-
-#define GIFIELD_GET_DROP_TYPE(field) ((field)&0x1F)
-#define GIFIELD_20 (1 << 5)
-#define GIFIELD_40 (1 << 6)
-#define GIFIELD_NO_COLLECTIBLE (1 << 7)
-/**
- * `flags` must be 0, GIFIELD_20, GIFIELD_40 or GIFIELD_NO_COLLECTIBLE (which can be or'ed together)
- * `dropType` must be either a value from the `Item00Type` enum or 0 if the `GIFIELD_NO_COLLECTIBLE` flag was used
- */
-#define GIFIELD(flags, dropType) ((flags) | (dropType))
-#define GI_START_TEXT 0xCE
-#define GID_CUSTOM GID_4C
-
-//This should be 0x50
-#define LAST_REGULAR_ACTION_ITEM ITEM_SWORD_DEITY
-#define NEW_ACTION_ITEMS (ITEM_CC+1)
-#define ITEM_BOMBMINE NEW_ACTION_ITEMS
-#define NUM_NEW_ITEMS 0x20
-#define NEW_ACTION_NUMBERS PLAYER_IA_MAX
-#define PLAYER_IA_BOMBMINE NEW_ACTION_NUMBERS
-#define MESSAGE_ICON_NEW EZTR_ICON_NOTHING_51
-#define MAX_REGULAR_SLOTS 0x30
-#define NUM_NEW_INV_SLOTS NUM_NEW_ITEMS
-
-extern s8 sItemItemActions[];
-extern PlayerUpperActionFunc sItemActionUpdateFuncs[PLAYER_IA_MAX];
-extern PlayerItemActionInitFunc sItemActionInitFuncs[PLAYER_IA_MAX];
-extern u8 sActionModelGroups[PLAYER_IA_MAX];
-extern s32 sPlayerUseHeldItem;
-extern s32 sPlayerHeldItemButtonIsHeldDown;
-extern PlayerAnimationHeader* D_8085BE84[PLAYER_ANIMGROUP_MAX][PLAYER_ANIMTYPE_MAX];
-extern GetItemEntry sGetItemTable[GI_MAX - 1];
-extern DrawItemTableEntry sDrawItemTable[];
-
-extern s32 D_801F6B08;
-
-extern Color_RGB8 D_801CFDEC[];
-extern s16 D_801CFE04[];
-extern s16 D_801CFE1C[];
-extern s16 D_801CFE34[];
-extern TexturePtr sStrayFairyIconTextures_code[];
-extern Color_RGB8 sStrayFairyIconPrimColors_code[];
-extern Color_RGB8 sStrayFairyIconEnvColors_code[];
-
-extern s16 D_801CFF70[LANGUAGE_MAX];
-extern s16 D_801CFF7C[LANGUAGE_MAX];
-extern s16 D_801CFF88[LANGUAGE_MAX];
-
-extern s16 D_801CFF94[];
-
-extern TexturePtr gHeartFullTex;
-extern TexturePtr gRupeeCounterIconTex;
-extern TexturePtr gStrayFairyGlowingCircleIconTex;
-
-s16 ItemExtension_ToNewItemRange(s16 itemID);
-
-s16 ItemExtension_FromItemRangeToItemID(s16 itemID);
 
 
-NewItemNum InitializeNewItemFromEntry(CustomItemEntry* entry);
 
-RECOMP_CALLBACK(".", init_items_event)
+RECOMP_CALLBACK(".", ItemExtension_init_items_event)
 void init_bombmine() {
-    recomp_printf("Callback Called!");
     sBombmineIN = InitializeNewItemFromEntry(&bombmineEntry);
-    //gEntryGI = GET_ITEM(ITEM_BOMBCHUS_20, OBJECT_GI_BOMB_2, GID_BOMBCHU, 0x2E, GIFIELD(GIFIELD_40 | GIFIELD_NO_COLLECTIBLE, 0), CHEST_ANIM_SHORT);
-    //gEntryGI.itemId = ItemExtension_FromItemRangeToItemID(sBombmineIN);
 }
 
-s32 ItemExtension_OfferExtendedGetItem(Actor* actor, PlayState* play, s16 getItemIdEx, f32 xzRange, f32 yRange);
-
-s32 ItemExtension_OfferExtendedGetItemFar(Actor* actor, PlayState* play, s16 getItemIdEx);
-
-s32 ItemExtension_OfferExtendedGetItemUnconditional(Actor* actor, PlayState* play, s16 getItemIdEx);
-
-#define SPECIAL_ITEM_CHEST_VALUE 0xFB
-#define SPECIAL_ITEM_CHEST_PARAM ((((u16)SPECIAL_ITEM_CHEST_VALUE))<<7)
 
 //////// REMOVE THESE ON RELEASE
 #include "overlays/actors/ovl_En_Guruguru/z_en_guruguru.h"
--- src/inventory_extension_core.c
+++ src/inventory_extension_core.c
@@ -1,118 +1,8 @@
-#include "modding.h"
-#include "global.h"
-#include "recomputils.h"
-#include "recompconfig.h"
-#include "eztr_api.h"
-
-#include "overlays/actors/ovl_En_Box/z_en_box.h"
+#include "chromeless_item_extension_common.h"
 
 #include "overlays/kaleido_scope/ovl_kaleido_scope/z_kaleido_scope.h"
-
 #include "z64message.h"
-#include "z64player.h"
-//#include "assets/objects/gameplay_keep/gameplay_keep.h"
 
-typedef void (*PlayerItemActionInitFunc)(PlayState*, Player*);
-
-s32 Player_UpperAction_CarryActor(Player* this, PlayState* play);
-s32 Player_UpperAction_ChangeHeldItem(Player* this, PlayState* play);
-
-#define NewItemNum s16
-
-typedef struct {
-    /* 0x0 */ void (*drawFunc)(PlayState*, s16);
-    /* 0x4 */ void* drawResources[8]; // Either display lists (Gfx*) or matrices (Mtx*)
-} DrawItemTableEntry;                 // size = 0x24
-
-typedef enum {
-    IT_CUSTOM = 0,
-    IT_MELEE_1H,
-    IT_MELEE_2H,
-    IT_USE,         //Immediately used items, like thrown Deku nuts
-    IT_BOTTLED,
-    IT_TRADE,
-    IT_VIEWFINDER,  //Items that you 'look through', e.g. pictograph, telescope
-    IT_PROJECTILE,  //Objects that 'shoot out of' something, uses first person + Z-targeting
-    IT_EXPLOSIVE,   //Actually just any items that produce something 'held above your head'
-    IT_SPELL,       //Magic with a casting animation
-    IT_MASK,
-    IT_TRANSFORMATION_MASK
-} ItemType;
-
-typedef enum {
-    UI_OOT_MM,
-    UI_OOT_ONLY,
-    UI_MM_ONLY
-} GamesUsedIn;
-
-typedef enum {
-    SA_AUTO_EMPTY = -1,     //for blank slots put there so the items can be collected later
-    SA_AUTO_PREFILL = -2,   //for the slot to be filled with the item unconditionally
-    SA_NONE = -3            //provides no slot assignment, mod should assign item using custom code
-} SlotAssignment;
-
-typedef struct {
-    PlayerItemActionInitFunc initFunc; //Used to initialize the item action
-    PlayerUpperActionFunc actionFunc;  //The item acton's update code
-    u8 modelGroup;                      //Should be a valid PlayerModelGroup
-    s16 slotAssignment;         //The inventory slot position number the item is to be assigned, otherwise SA_NONE gives no slot-
-                                //for moders who want to deal with that themselves, while SA_AUTO_(EMPTY/PREFILL) generates a new one
-} InventoryItemPerGameEntry;
-
-typedef struct {
-    char* itemName;             //Name the item will be refered to as internally, should be unique
-    GamesUsedIn usedIn;         //Will this item be used in OoT, MM or both?
-    TexturePtr icon;            //Pointer to RGBA32 Texture
-    TexturePtr nameLabelEng;    //Pointer to IA8 Texture
-
-    char* EZTR_KaleidoPopupText;        //Used in MM by any item assigned to a slot
-    char* EZTR_GiveItemText;            //Used for givables
-    InventoryItemPerGameEntry ocarinaFuncs;
-    InventoryItemPerGameEntry mujuraFuncs;
-    ItemType type;                      //optional, provides information to the integration code
-    DrawItemTableEntry* drawEntryGI;    //Needed for givable/tradable items, otherwise optional
-    s16 salePrice;                      //defaults to 0, meaning the item will not be sold, other numbers mean shops will accept it
-    char* metadata;                     //optional, arbitrary null terminated data used to communicate with other mods
-} CustomItemEntry;
-
-typedef struct GetItemEntry {
-    /* 0x0 */ u8 itemId;
-    /* 0x1 */ u8 field; // various bit-packed data
-    /* 0x2 */ s8 gid;   // defines the draw id and chest opening animation
-    /* 0x3 */ u8 textId;
-    /* 0x4 */ u16 objectId;
-} GetItemEntry; // size = 0x6
-
-#define IDLE_ANIM_NONE 0
-#define CHEST_ANIM_SHORT 0
-#define CHEST_ANIM_LONG 1
-
-// TODO: consider what to do with the NONEs: cannot use a zero-argument macro like OoT since the text id is involved.
-#define GET_ITEM(itemId, objectId, drawId, textId, field, chestAnim) \
-    { itemId, field, (chestAnim != CHEST_ANIM_SHORT ? 1 : -1) * (drawId + 1), textId, objectId }
-
-#define GIFIELD_GET_DROP_TYPE(field) ((field)&0x1F)
-#define GIFIELD_20 (1 << 5)
-#define GIFIELD_40 (1 << 6)
-#define GIFIELD_NO_COLLECTIBLE (1 << 7)
-/**
- * `flags` must be 0, GIFIELD_20, GIFIELD_40 or GIFIELD_NO_COLLECTIBLE (which can be or'ed together)
- * `dropType` must be either a value from the `Item00Type` enum or 0 if the `GIFIELD_NO_COLLECTIBLE` flag was used
- */
-#define GIFIELD(flags, dropType) ((flags) | (dropType))
-#define GI_START_TEXT 0xCE
-#define GID_CUSTOM GID_4C
-
-//This should be 0x50
-#define LAST_REGULAR_ACTION_ITEM ITEM_SWORD_DEITY
-#define NEW_ACTION_ITEMS (ITEM_CC+1)
-#define ITEM_BOMBMINE NEW_ACTION_ITEMS
-#define NUM_NEW_ITEMS 0x20
-#define NEW_ACTION_NUMBERS PLAYER_IA_MAX
-#define PLAYER_IA_BOMBMINE NEW_ACTION_NUMBERS
-#define MESSAGE_ICON_NEW EZTR_ICON_NOTHING_51
-#define MAX_REGULAR_SLOTS 0x30
-#define NUM_NEW_INV_SLOTS NUM_NEW_ITEMS
 
 extern s8 sItemItemActions[];
 extern PlayerUpperActionFunc sItemActionUpdateFuncs[PLAYER_IA_MAX];
@@ -128,7 +18,7 @@ u16 currentNewItemTotal = 0;
 u16 currentNextFreeSlot = MAX_REGULAR_SLOTS;
 s8 gNewItemActions[NUM_NEW_ITEMS] = {};
 PlayerItemActionInitFunc gNewItemActionInitFuncs[NUM_NEW_ITEMS] = {};
-PlayerUpperActionFunc gNewItemActionUpdateFuncs[NUM_NEW_ITEMS] = {};//Use Player_UpperAction_CarryActor
+PlayerUpperActionFunc gNewItemActionUpdateFuncs[NUM_NEW_ITEMS] = {};
 TexturePtr gNewItemIcons[NUM_NEW_ITEMS];
 TexturePtr gNewItemNames[NUM_NEW_ITEMS];
 u8 sNewActionModelGroups[NUM_NEW_ITEMS];
@@ -169,10 +59,12 @@ extern TexturePtr gHeartFullTex;
 extern TexturePtr gRupeeCounterIconTex;
 extern TexturePtr gStrayFairyGlowingCircleIconTex;
 
+RECOMP_EXPORT
 s16 ItemExtension_ToNewItemRange(s16 itemID) {
     return itemID - NEW_ACTION_ITEMS;
 }
 
+RECOMP_EXPORT
 s16 ItemExtension_FromItemRangeToItemID(s16 itemID) {
     return itemID + NEW_ACTION_ITEMS;
 }
@@ -252,6 +144,8 @@ void Core_Replace_Get_Text(s16 newEntryNum, char* EZTR_text) {
 GetItemEntry gEntryGI = GET_ITEM(ITEM_BOMBCHUS_20, OBJECT_GI_BOMB_2, GID_BOMBCHU, 0x2E , GIFIELD(GIFIELD_20 | GIFIELD_NO_COLLECTIBLE, 0), CHEST_ANIM_LONG);
 GetItemId gAlteredGI = GI_46; /*GI_MASK_CIRCUS_LEADER;*///GI_DEED_LAND;//
 
+
+RECOMP_EXPORT
 NewItemNum InitializeNewItemFromEntry(CustomItemEntry* entry) {
     if (currentNewItemTotal >= NUM_NEW_ITEMS)
         return -1;
@@ -275,13 +169,13 @@ NewItemNum InitializeNewItemFromEntry(CustomItemEntry* entry) {
     return currentNewItemTotal++;
 }
 
-RECOMP_DECLARE_EVENT(init_items_event());
+RECOMP_DECLARE_EVENT(ItemExtension_init_items_event());
 
 EZTR_ON_INIT void ETZR_Item_Expansion_function() {
     for (s16 ii = 0; ii < NUM_NEW_INV_SLOTS; ii++) {
         gNewInventoryItemSlots[ii] = ITEM_NONE;
     }
-    init_items_event();
+    ItemExtension_init_items_event();
 }
 
 RECOMP_HOOK("Player_InitCommon") void setup_inventory(Player* this, PlayState* play, FlexSkeletonHeader* skelHeader) {
@@ -310,6 +204,7 @@ RECOMP_HOOK("Player_InitCommon") void setup_inventory(Player* this, PlayState* p
     }
 }
 
+RECOMP_EXPORT
 s32 ItemExtension_OfferExtendedGetItem(Actor* actor, PlayState* play, s16 getItemIdEx, f32 xzRange, f32 yRange) {
     gEntryGI.itemId = ItemExtension_FromItemRangeToItemID(getItemIdEx);
     gEntryGI.textId = GI_START_TEXT+getItemIdEx;
@@ -321,10 +216,12 @@ s32 ItemExtension_OfferExtendedGetItem(Actor* actor, PlayState* play, s16 getIte
     return Actor_OfferGetItem(actor, play, gAlteredGI, xzRange, yRange);
 }
 
+RECOMP_EXPORT
 s32 ItemExtension_OfferExtendedGetItemFar(Actor* actor, PlayState* play, s16 getItemIdEx) {
     return ItemExtension_OfferExtendedGetItem(actor, play, getItemIdEx, 9999.9f, 9999.9f);
 }
 
+RECOMP_EXPORT
 s32 ItemExtension_OfferExtendedGetItemUnconditional(Actor* actor, PlayState* play, s16 getItemIdEx) {
     GetItemId getItemId = gAlteredGI;
     gEntryGI.itemId = ItemExtension_FromItemRangeToItemID(getItemIdEx);
